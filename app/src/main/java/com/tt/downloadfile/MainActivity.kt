ca06f600d9e7909c1c406c545f1a3beb476af18f
thanhbat96 <thanhbat96@gmail.com>
2020-03-10 18:46:10 +0700

    move to AsyncTask

--- app/src/main/java/com/tt/downloadfile/MainActivity.kt
+++ app/src/main/java/com/tt/downloadfile/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.tt.downloadfile
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import android.widget.Toast
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.tt.downloadfile.databinding.ActivityMainBinding
 import com.tt.downloadfile.viewmodel.DownloadViewModel
 import okhttp3.ResponseBody
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.*
+import java.net.URL
+import java.net.URLConnection
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val downloadViewModel: DownloadViewModel by viewModel()
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +34,45 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonDownload.setOnClickListener {
-            downloadViewModel.downloadFile(token, "Annotation 2020-03-10 021514.png")
-            downloadViewModel.byteArray.observe(this, Observer { response ->
-                val writtenToDisk: Boolean = writeResponseBodyToDisk(response)
-                Toast.makeText(this, "Download", Toast.LENGTH_SHORT).show()
-            })
+            object : AsyncTask<String?, Int?, String?>() {
+                override fun onPreExecute() {
+                    progressBar = binding.progressHorizontal
+                    progressBar.max = 100
+                }
+
+                override fun doInBackground(vararg params: String?): String? {
+                    var url: URL = URL(params[0])
+                    var con: URLConnection = url.openConnection()
+                    con.connect()
+                    var fileLeng = con.contentLength
+                    var filePath =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
+
+                    var inputStream = BufferedInputStream(url.openStream())
+                    var outputStream =
+                            FileOutputStream("/storage/emulated/0/Android/data/com.tt.downloadfile/files/Download" + "/Annotation 2020-03-10 021514.png")
+                    val data = ByteArray(1024)
+                    var total = 0
+                    var count: Int
+                    while (inputStream.read(data).also { count = it } !== -1) {
+                        total += count
+                        publishProgress(total * 100 / fileLeng)
+                        outputStream.write(data, 0, count)
+                    }
+
+                    outputStream.flush()
+                    outputStream.close()
+                    inputStream.close()
+                    return null
+                }
+
+                override fun onProgressUpdate(vararg values: Int?) {
+                    super.onProgressUpdate(*values)
+                    values[0]?.let { it1 -> progressBar.setProgress(it1) }
+                }
+            }.execute("https://handsomeman.herokuapp.com/api/jobs/downloadFile/Annotation%202020-03-10%20021514.png")
+//                val writtenToDisk: Boolean = writeResponseBodyToDisk(response)
+//                Toast.makeText(this, "Download", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -78,6 +115,4 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
-
 }
-
